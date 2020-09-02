90421371beb370662048a92f4b3b16a79479c3a9
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-02 10:31:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ bot.on('message', async message => {
 		.setTitle('INVITE')
 		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.setDescription('https://discord.com/oauth2/authorize?client_id=750547209002680431&scope=bot')
+		.setDescription(" [``[Click here]``](https://discord.com/oauth2/authorize?client_id=750547209002680431&scope=bot)")
 		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.setTimestamp()
 		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
