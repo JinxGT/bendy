d4a6a473502c0dbb54e55151f7242dd5ad4552b0
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-01 21:19:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,4 +41,22 @@ bot.on('message', async message => {
 	
 })
 
+// THIS IS THE (be!invite) COMMAND
+
+bot.on('message', async message => {
+	if (message.content === "be!invite" ) {
+	const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#383fff')
+		.setTitle('INVITE')
+		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setDescription('https://discord.com/oauth2/authorize?client_id=750547209002680431&scope=bot')
+		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+	}	
+	
+})
+
 bot.login(process.env.token);
