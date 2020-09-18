dc06d693b0d3386376e8dfa227e5feb6fdfda223
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-18 07:23:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -201,56 +201,50 @@ bot.on('message', async message => {
 	
 })
 
-// THIS IS THE BAN COMMAND
+// THIS IS THE KICK AND BAN COMMANDS COMMAND
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+bot.on("message", (message) => {
+    if (message.content.startsWith("be!kick")) {
+        if(!message.member.roles.find("name", "MODS"))
+            return;
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
 
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
+        // No mentions catch
+        if (member === undefined) return;
 
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("You didn't mention the user to kick!");
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
     }
-  }
 });
+
+bot.on("message", (message) => {
+    if (message.content.startsWith("be!ban")) {
+        if(!message.member.roles.find("name", "MODS"))
+            return;
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+
+        // No mentions catch
+        if (member === undefined) return;
+
+        // Ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
+
 // THIS IS THE TOKEN HOLDER
 
 bot.login(process.env.token);
