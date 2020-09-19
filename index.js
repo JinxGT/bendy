a810b8b6bcd8a467c4db180bab7ab19e9b7e9bc3
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-19 09:15:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,38 @@ bot.on('message', async message => {
 })
 
 bot.on('message', async message => {
+	if (message.content === "be!coinflip" ) {
+	const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#383fff')
+		.setTitle('Coin Flip')
+		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setDescription('COMING SOON!')
+		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+	}	
+	
+})
+
+bot.on('message', async message => {
+	if (message.content === "be!8ball" ) {
+	const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#383fff')
+		.setTitle('8BALL')
+		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setDescription('COMING SOON!')
+		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+	}	
+	
+})
+
+bot.on('message', async message => {
 	if (message.content === "be!status" ) {
 	const exampleEmbed = new Discord.MessageEmbed()
 		.setColor('#383fff')
