f192f40bec9e4e2ddc1c5ad171215373746a8dd1
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-09-19 08:59:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ bot.on('message', async message => {
 		.setDescription('To invite this bot to your server use the ``be!invite`` command!')
 		.addField("⏳ ping", `${Math.round(bot.ws.ping)}ms`, false)
 		.addField(":globe_with_meridians: Servers", `${bot.guilds.array().length}`, false)
+		.addField(":yellow_circle: Status", `Possible downtime and latency!`, false)
 		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.setTimestamp()
 		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
@@ -61,6 +62,22 @@ bot.on('message', async message => {
 		.setTitle('PONG!')
 		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.addField("⏳ Your ping is:", `${Math.round(bot.ws.ping)}ms`, false)
+		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setTimestamp()
+		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
+			
+		message.channel.send(exampleEmbed);
+	}	
+	
+})
+
+bot.on('message', async message => {
+	if (message.content === "be!status" ) {
+	const exampleEmbed = new Discord.MessageEmbed()
+		.setColor('#383fff')
+		.setTitle('PONG!')
+		.setAuthor('Bendy', 'https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
+		.setDescription(':yellow_circle:: Possible downtime and latency!')
 		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.setTimestamp()
 		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
@@ -102,6 +119,7 @@ bot.on('message', async message => {
 		.addField('Invite', '``be!invite``', false)
 		.addField('Stats', '``be!stats``', false)
 		.addField('Ping', '``be!ping``', false)
+		.addField('Status', '``be!status``', false)
 		.setThumbnail('https://media.discordapp.net/attachments/750544950860447764/750546624694190090/JPEG_20200721_071825.jpg')
 		.setTimestamp()
 		.setFooter('Wampus Development Group', 'https://media.discordapp.net/attachments/749691775202885645/750474651389526097/463d17316ea53baf574535c84b88c525.png?width=401&height=401');
